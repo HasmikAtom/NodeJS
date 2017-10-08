2822d5ca302879c9bf65c484ce023b89b075369d
Hasmik <hasmikatomyan@gmail.com>
2017-10-09 03:34:21 +0400

    functions

--- app2.js
+++ app2.js
@@ -3,66 +3,120 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  let defEndPoint = '/tweets';
-  let tweetPath = './tweets.json'
-  let checkFile = fs.existsSync('./tweets.json')
+//functions
+const readBody = (req, callback) =>{
+  let bodyArray = [];
+  req.on('error', (err) => {
+    console.error(err);
+  }).on('data', (chunk)=>{
+    bodyArray.push(chunk);
+  }).on('end', () =>{
+    bodyArray = Buffer.concat(bodyArray).toString(); //buffer into string
+    bodyArray = JSON.parse(bodyArray) //object looking string into json
+    callback(bodyArray)
+  })
+}
 
-// let bla = [{'astf':'dasg'}, {'asagah':'hrert'}]
-// let ble = [{546: 'ywey'}, {6876: 'asafwtw'}]
+const fileRead = (path, callback) =>{
+    fs.readFile(path, 'utf8', (err, data) =>{
+      if(err) throw err
+      callback(data)
+    })
+}
+
+const fileWrite = (path, data) => {
+  fs.writeFile(path, JSON.stringify(data), (err)=>{
+    if(err) throw err
+  })
+}
+
+//Server start
+
 //
-// let blable = JSON.stringify(bla.concat(ble));
-// console.log('concat array' + blable)
-
+const server = http.createServer((req, res) => {
   const {method, url, headers } = req
+  let defEndPoint = '/api/tweets';
+  let tweetPath = './tweets.json'
+
 
   if(url !== defEndPoint) res.end('to got tweets')
 
   else if (url === defEndPoint){
 
     if(method === 'POST'){
-      let bodyArray = [];
-      req.on('error', (err) => {
-        console.error(err);
-      }).on('data', (chunk)=>{
-        // console.log(chunk)
-        bodyArray.push(chunk);
-      }).on('end', ()=>{
-
-        bodyArray = Buffer.concat(bodyArray).toString(); //buffer into string
-        bodyArray = JSON.parse(bodyArray) //object looking string into json
-
-          fs.readFile(tweetPath, 'utf8', (err,data) =>{ // read file
-            if(err) throw err;
-            let id = `${new Date().valueOf()}`
-            bodyArray = bodyArray.map(tweet =>{
-              id -= 10
-              tweet = Object.assign({},tweet,{id})
-              // tweet = {'id': id, 'tweet':tweet.tweet, 'user':tweet.user}
-              return tweet
-            })
-            let obj = {}
-            if(!data){
-              obj = {'tweets':[]}
-            }else{
-              obj = JSON.parse(data)
-            }
-            const oldTweets = obj.tweets
-            const newTweets = oldTweets.concat(bodyArray)
-            console.log(newTweets)
-            let newObj = {'tweets': newTweets}
-            newObj = JSON.stringify(newObj, null, '\t')
-            fs.writeFile(tweetPath, newObj, (err)=> { // use append
-              if(err) throw err;
-            })
-          })
+      // let bodyArray = [];
+      // req.on('error', (err) => {
+      //   console.error(err);
+      // }).on('data', (chunk)=>{
+      //   bodyArray.push(chunk);
+      // }).on('end', ()=>{
+      //
+      //   bodyArray = Buffer.concat(bodyArray).toString(); //buffer into string
+      //   bodyArray = JSON.parse(bodyArray) //object looking string into json
+      readBody(req, (bodyArray)=>{
+        let id = `${new Date().valueOf()}`
+        bodyArray = bodyArray.map(tweet =>{
+          id -= 10
+          tweet = Object.assign({},tweet,{id})
+          return tweet
       })
-    } else if(method === 'GET'){
-      fs.readFile(tweetPath, (err, data) =>{
-        if(err) throw err;
-        res.write(data.toString());
-        res.end()
+        fileRead(tweetPath, (data)=>{
+          let obj = {}
+          if(!data){
+            console.log('data doesnt exists')
+            obj = {'tweets':[]}
+            console.log('obj before posting to file', obj.tweets) //empty array
+          }else{
+            obj = JSON.parse(data.toString()) //
+            console.log('data exists')
+            // console.log('obj data', obj)
+            // console.log('data without parse', data)
+            // console.log('obj.tweets', obj.tweets)
+          }
+          const oldTweets = obj.tweets
+          console.log('oldtweets', typeof oldTweets)
+          const newTweets = oldTweets.concat(bodyArray)
+          console.log('newTweets', newTweets)
+          let newObj = {'tweets': newTweets}
+          newObj = JSON.parse(JSON.stringify(newObj,null,'\t'))
+          fileWrite(tweetPath, newObj)
+        })
+          // fs.readFile(tweetPath, 'utf8', (err,data) =>{ // read file
+            // if(err) throw err;
+            // let id = `${new Date().valueOf()}`
+            // bodyArray = bodyArray.map(tweet =>{
+            //   id -= 10
+            //   tweet = Object.assign({},tweet,{id}) // creates object and adds tweets and id to it
+            //   // tweet = {'id': id, 'tweet':tweet.tweet, 'user':tweet.user}
+            //   return tweet
+            // })
+            // let obj = {}
+            // if(!data){
+            //   obj = {'tweets':[]}
+            // }else{
+            //   obj = JSON.parse(data)
+            // }
+            // const oldTweets = obj.tweets
+            // console.log('oldtweets', oldTweets)
+            // const newTweets = oldTweets.concat(bodyArray)
+            // console.log('newTweets', newTweets)
+            // let newObj = {'tweets': newTweets}
+            // newObj = JSON.stringify(newObj, null, '\t')
+            // fs.writeFile(tweetPath, newObj, (err)=> { // use append
+            //   if(err) throw err;
+            // })
+          }) //readbody end
+    //  })
+  } else if(method === 'GET'){ // method check end
+      fileRead(tweetPath, (data) =>{
+        res.writeHead(200, {'Content-Type': 'application/json'})
+        res.end(data)
       })
+      // fs.readFile(tweetPath, (err, data) =>{
+      //   if(err) throw err;
+      //   res.write(data.toString());
+      //   res.end()
+      // })
     }
   }
 
